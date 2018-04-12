605f651257be2bcabc096008ea43ff184fc72359
David Klopp <dklopp@students.uni-mainz.de>
2018-04-12 10:40:19 +0200

    Remove matrix struct

--- cannon/cannon.cpp
+++ cannon/cannon.cpp
@@ -1,207 +1,14 @@
 #include "../include/matrix_helper.hpp"
+#include "../include/cannon.hpp"
 
 // configure datatypes to use
-#define MPI_VALUE_TYPE MPI_INT
-typedef int data_t;
-
-
-template <
-    typename value_t>
-void naive_multiply_add(int size, value_t *A, value_t *B, value_t *C)
-{
-  // simple matrix multiplication C = C + A*B
-  //#pragma omp parallel for schedule(dynamic) collapse(2)
-  for (int i = 0; i < size; ++i) {
-    for (int j = 0; j < size; ++j) {
-      int s = 0;
-      for (int k = 0; k < size; ++k) {
-        s += A[i*size+k] * B[k*size+j];
-      }
-      C[i*size+j] += s;
-    }
-  }
-}
-
-
-template <
-    typename value_t>
-void multiply_parallel(matrix<value_t> &A,
-                       matrix<value_t> &B,
-                       matrix<value_t> &C)
-{
-  // save the id of the current process and the total number of processes
-  int num_proc, my_id;
-  MPI_Comm_size(MPI_COMM_WORLD, &num_proc);
-  MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
-
-  // algorithm requires sqaure matrices
-  const int N = A.rows;
-  if (!my_id) assert (B.rows == N && B.cols == N && A.cols == N);
-
-  // amount of blocks in one row / column of the matrix
-  const int grid_size = (int)sqrt(num_proc);
-  // amount of columns / rows in one matrix block (pad with zeros)
-  const int extra_space = N%grid_size ? grid_size - (N % grid_size) : 0;
-  const int block_size = (N + extra_space)/grid_size;
-  // number of elements in one matrix block
-  const int local_matrix_size = block_size*block_size;
-
-
-  // create 2D sqrt(p) x sqrt(p) grid communicator
-  const int periods[2] = {1, 1}; // periodic in both dimensions
-  const int dims[2] = {grid_size, grid_size}; // size of each dimension
-  MPI_Comm grid_comm;
-  MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &grid_comm);
-
-  // local matrices
-  value_t *local_A = new value_t[local_matrix_size];
-  value_t *local_B = new value_t[local_matrix_size];
-  value_t *local_C = new value_t[local_matrix_size];
-  // fill matrices with zeros before adding values to it
-  std::fill_n(local_C, local_matrix_size, 0);
-  std::fill_n(local_A, local_matrix_size, 0);
-  std::fill_n(local_B, local_matrix_size, 0);
-
-  // get coordinates of process inside the 2D grid
-  int coords[2] = {0, 0};
-  MPI_Cart_coords(grid_comm, my_id, grid_size, coords);
-  // all indices smaller than this number need to send less data
-  const int adjust_indices = (grid_size - (N % grid_size)) % grid_size;
-  // datatypes and counts + displacements for send and receive
-  MPI_Datatype TMP,
-    BLOCK[num_proc], // send data from A or B to local matrix
-    LOCAL_BLOCK[num_proc], // Receive local_A or local_B from A or B
-    GATHER_LOCAL[num_proc]; // Gather local_C in C
-  int *displs = new int[num_proc];
-  int row_offset = 0, col_offset = 0;
-
-  for (int row = 0; row < grid_size; ++row) {
-    col_offset = 0;
-    const int off_row = (row < adjust_indices);
-    for (int col = 0; col < grid_size; ++col) {
-      // ignore padding for first columns when sending the data
-      const int off_col = (col < adjust_indices);
-      const int p_id = row*grid_size+col;
-
-      // create datatype to send submatrices (BLOCK)
-      MPI_Type_vector(block_size-off_row, block_size-off_col, N, MPI_VALUE_TYPE, &TMP);
-      MPI_Type_create_resized(TMP , 0, sizeof(value_t), &BLOCK[p_id]);
-      MPI_Type_commit(&BLOCK[p_id]);
-
-      // create datatype to receive submatrices (LOCAL_BLOCK)
-      MPI_Type_vector(block_size-off_row, block_size-off_col, block_size, MPI_VALUE_TYPE, &TMP);
-      MPI_Type_create_resized(TMP , 0, sizeof(value_t), &LOCAL_BLOCK[p_id]);
-      MPI_Type_commit(&LOCAL_BLOCK[p_id]);
-
-      // create datatype to receive submatrices in C (GATHER_LOCAL)
-      MPI_Type_vector(block_size-off_row, block_size-off_col, N, MPI_VALUE_TYPE, &TMP);
-      MPI_Type_create_resized(TMP , 0, sizeof(value_t), &GATHER_LOCAL[p_id]);
-      MPI_Type_commit(&GATHER_LOCAL[p_id]);
-
-      // displacement for send operation
-      displs[p_id] = row_offset + col_offset;
-      // increase column offset
-      col_offset += block_size-off_col;
-    }
-    // increase row offset
-    row_offset += N*(block_size-off_row);
-  }
-
-  // scatter data so that each process (i,j) has the entries for the
-  // matrix block at index (i, j)
-  MPI_Request req;
-  if (!my_id) {
-    for (int i = 0; i < num_proc; ++i) {
-      MPI_Isend(A.data+displs[i], 1, BLOCK[i], i, 0, grid_comm, &req);
-      MPI_Isend(B.data+displs[i], 1, BLOCK[i], i, 1, grid_comm, &req);
-    }
-  }
-  MPI_Recv(local_A, 1, LOCAL_BLOCK[my_id], 0, 0, grid_comm, MPI_STATUS_IGNORE);
-  MPI_Recv(local_B, 1, LOCAL_BLOCK[my_id], 0, 1, grid_comm, MPI_STATUS_IGNORE);
-
-  // wait until all data is send
-  //MPI_Barrier(grid_comm);
-  /*
-  After inital scatter:
-  Example blocks for matrix A or B (the numbers denote the process ids):
-  0,0  |  0, 1  |  0, 2
-  1,0  |  1, 1  |  1, 2
-  2,0  |  2, 1  |  2, 2
-  */
-
-  // ids of neighbours
-  int right=0, left=0, down=0, up=0;
-  // shift A based on the row and B based on the current column
-  MPI_Cart_shift(grid_comm, 1, coords[0], &left, &right);
-  MPI_Cart_shift(grid_comm, 0, coords[1], &up, &down);
-  MPI_Sendrecv_replace(local_A, local_matrix_size, MPI_VALUE_TYPE, left,
-    0, right, 0, grid_comm, MPI_STATUS_IGNORE);
-  MPI_Sendrecv_replace(local_B, local_matrix_size, MPI_VALUE_TYPE, up,
-    0, down, 0, grid_comm, MPI_STATUS_IGNORE);
-
-  /*
-  After inital shift:
-  Example blocks for matrix A:       Example blocks for matrix B:
-  0,0  |  0, 1  |  0, 2              0,0  |  1, 1  |  2, 2
-  1,1  |  1, 2  |  1, 0              1,0  |  2, 1  |  0, 2
-  2,2  |  2, 0  |  2, 1              2,0  |  0, 1  |  1, 2
-  */
-
-  // multiply and add values to C = C + A*B
-  naive_multiply_add(block_size, local_A, local_B, local_C);
-
-  // shift values of A/B to left/up in a loop
-  for(int i = 1; i < grid_size; ++i) {
-     MPI_Cart_shift(grid_comm, 1, 1, &left,&right);
-     MPI_Cart_shift(grid_comm, 0, 1, &up,&down);
-     MPI_Sendrecv_replace(local_A, local_matrix_size, MPI_VALUE_TYPE, left,
-       0, right, 0, grid_comm, MPI_STATUS_IGNORE);
-     MPI_Sendrecv_replace(local_B, local_matrix_size, MPI_VALUE_TYPE, up,
-       0, down, 0, grid_comm, MPI_STATUS_IGNORE);
-     naive_multiply_add(block_size, local_A, local_B, local_C);
-  }
-
-  // gather values in C for final result
-  MPI_Isend(local_C, 1, LOCAL_BLOCK[my_id], 0, 0, grid_comm, &req);
-  if (!my_id) {
-    for (int i = 0; i < num_proc; ++i) {
-      MPI_Recv(C.data+displs[i], 1, GATHER_LOCAL[i], i, 0, grid_comm,
-        MPI_STATUS_IGNORE);
-    }
-  }
-
-  // cleanup
-  delete[] displs;
-  delete[] local_A;
-  delete[] local_B;
-  delete[] local_C;
-}
-
-
-template <
-    typename value_t>
-void multiply(matrix<value_t> &A,
-              matrix<value_t> &B,
-              matrix<value_t> &C) // output matrix
-{
-  // simple matrix multiplication C = A*B
-  //#pragma omp parallel for schedule(dynamic) collapse(2)
-  for (int i = 0; i < A.rows; ++i) {
-    for (int j = 0; j < B.cols; ++j) {
-      value_t s = 0;
-      for (int k = 0; k < A.rows; ++k) {
-        s += A(i, k) * B(k, j);
-      }
-      C(i, j) = s;
-    }
-  }
-}
+typedef float data_t;
 
 
 int main(int argc, char* argv[])
 {
   // size of matrix
-  int n = 1<<4;
+  int n = 1<<10;
   // Init MPI
   MPI_Init(&argc, &argv);
   int num_proc;
@@ -214,64 +21,50 @@ int main(int argc, char* argv[])
 
   // processes can be arranged in a grid
   assert (size*size == num_proc);
-  // matrix can be distributed evenly
-  //assert ((n*n)%size == 0);
-
-  // create example matricies
-  typedef matrix<data_t> matrix_t;
 
   // placeholder for matrices
-  data_t *data_C = nullptr, *data_A = nullptr, *data_B = nullptr;
-  matrix_t C2, C, A, B;
-  // set rows and colums for all processes
-  A.rows = n; A.cols = n;
-  B.rows = n; B.cols = n;
+  data_t *data_C = nullptr,
+         *data_A = nullptr,
+         *data_B = nullptr,
+         *data_C2 = nullptr;
 
   if (!my_id) {
     // create matrices to multiply
     data_A = new data_t[n*n];
-    A = matrix_t(data_A, n, n);
-    A.fill_rnd_int();
-    //A.print();
+    fill_rnd(data_A, n);
 
     data_B = new data_t[n*n];
-    B = matrix_t(data_B, n, n);
-    B.fill_rnd_int();
-    //B.print();
+    fill_rnd(data_B, n);//_int();
 
     // create target matrix for serial execution
     data_C = new data_t[n*n];
-    C = matrix_t(data_C, n, n);
-    C.fill_zero();
+    fill_zero(data_C, n);
 
     // create target matrix for parallel execution
-    data_C = new data_t[n*n];
-    C2 = matrix_t(data_C, n, n);
-    C2.fill_zero();
+    data_C2 = new data_t[n*n];
+    fill_zero(data_C2, n);
   }
 
   // serial: multiply matrix A and B
   double start = MPI_Wtime();
-  if(!my_id) multiply<data_t>(A, B, C);
+  if(!my_id) naive_multiply_add(n, data_A, data_B, data_C);
   double end = MPI_Wtime();
 
   if(!my_id) {
     printf("# elapsed time (serial_scoring_matrix): %fs\n", end-start);
-    //C.print();
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
 
   // parallel: multiply matrix A and B
   start = MPI_Wtime();
-  multiply_parallel<data_t>(A, B, C2);
+  multiply_parallel<data_t>(n, data_A, data_B, data_C2);
   end = MPI_Wtime();
 
   if(!my_id) {
-    if (!C.compare(C2))
+    if (compare(data_C, data_C2, n))
       std::cout << "Parallel and serial results are different!" << std::endl;
     printf("# elapsed time (parallel_scoring_matrix): %fs\n", end-start);
-    //C2.print();
   }
 
   MPI_Barrier(MPI_COMM_WORLD);
--- include/matrix_helper.hpp
+++ include/matrix_helper.hpp
@@ -1,3 +1,9 @@
+/*
+Helper class for serial execution.
+*/
+#ifndef MATRIX_HELPER_HPP
+#define MATRIX_HELPER_HPP
+
 #include <random>
 #include <iostream>
 #include <mpi.h>
@@ -5,73 +11,62 @@
 
 template <
     typename value_t>
-struct matrix {
-
-    int rows;
-    int cols;
-    value_t *data;
-
-    matrix() {}
-
-    matrix(value_t *data_, int rows_, int cols_) :
-      data(data_),
-      rows(rows_),
-      cols(cols_) {}
-
-    void print() {
-      for(int y = 0; y < this->rows; ++y) {
-          for(int x = 0; x < this->cols; ++x)
-              std::cout <<  this->data[y*this->rows +x] << " \t" ;
-          std::cout << std::endl;
-      }
+void print_matrix(value_t A, int N) {
+  for(int x = 0; x < N; ++x) {
+      for(int y = 0; y < N; ++y)
+          std::cout <<  A[x*N +y] << " \t" ;
       std::cout << std::endl;
-    }
+  }
+  std::cout << std::endl;
+}
 
-    void fill_rnd_int() {
-      std::random_device rnd;
-      // mt19937 is faster than random_device() which does not need any seeds
-      std::mt19937 mt(rnd());
-      // Create random numbers in [1, 4294967296)
-      std::uniform_int_distribution<value_t> dist(-1, 3);
-      for(int y = 0; y < this->rows; y++)
-          for(int x = 0; x < this->cols; x++)
-              this->data[x + y*cols] = dist(mt);
-    }
 
-    void fill_rnd() {
-      std::random_device rnd;
-      // mt19937 is faster than random_device() which does not need any seeds
-      std::mt19937 mt(rnd());
-      // Create random numbers in [1, 4294967296)
-      std::uniform_real_distribution<value_t> dist(-1, 3);
-      for(int y = 0; y < this->rows; y++)
-          for(int x = 0; x < this->cols; x++)
-              this->data[x + y*cols] = dist(mt);
-    }
+template <
+    typename value_t>
+void fill_rnd(value_t *A, int N) {
+  std::random_device rnd;
+  // mt19937 is faster than random_device() which does not need any seeds
+  std::mt19937 mt(rnd());
+  // Create random numbers in [1, 4294967296)
+  std::uniform_real_distribution<value_t> dist(-1, 3);
+  for(int x = 0; x < N; ++x)
+      for(int y = 0; y < N; ++y)
+          A[x*N + y] = dist(mt);
+}
 
-    void fill_zero() {
-      std::fill_n(this->data, this->rows * this->cols, 0);
-    }
 
-    value_t &operator()(const int& row, const int& col) {
-      return this->data[this->rows*row + col];
-    }
+template <
+    typename value_t>
+void fill_rnd_int(value_t *A, int N) {
+  std::random_device rnd;
+  // mt19937 is faster than random_device() which does not need any seeds
+  std::mt19937 mt(rnd());
+  // Create random numbers in [1, 4294967296)
+  std::uniform_int_distribution<value_t> dist(-1, 3);
+  for(int x = 0; x < N; ++x)
+      for(int y = 0; y < N; ++y)
+          A[x*N + y] = dist(mt);
+}
 
-    value_t &operator[](const int idx) {
-      // get a value from the matrix at the absolut array index
-      return this->data[idx];
-    }
 
-    bool compare(matrix<value_t> &B) {
-      // return true if both matrices are the same, otherwise false
-      assert (this->rows == B.rows && this->cols == B.cols);
-      for (int i = 0; i < this->rows; ++i) {
-        for (int j = 0; j < this->cols; ++j) {
-          if ((*this)(i, j) != B(i, j)) {
-            return false;
-          }
-        }
+template <
+    typename value_t>
+void fill_zero(value_t *A, int N) {
+  std::fill_n(A, N*N, 0);
+}
+
+
+template <
+    typename value_t>
+bool compare(value_t *A, value_t *B, int N) {
+  // return true if both matrices are the same, otherwise false
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      if (A[i*N+j] != B[i*N+j]) {
+        return false;
       }
-      return true;
     }
-};
+  }
+  return true;
+}
+#endif // MATRIX_HELPER_HPP
